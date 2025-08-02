6a9d0a5deec6418c86e89757bd130f32fc6fa3d8
HaoTNG <nguyentronghao13579@gmail.com>
2025-08-02 17:16:45 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <h1 className="text-amber-400 font-bold text-4xl underline">
+            hi bro
+        </h1>
+    </>
+
+  )
+}
+
+export default App
